bd23cb00dd6f429540833687ba9d448ec93c24e1
Joshua L.J. Vachon <joshua.vachon@cyberfjord.studio>
2023-11-17 07:20:11 -0500

    Ajout Py

---
+++ ocr/main.py
@@ -0,0 +1,11 @@
+def main():
+    print("Choix disponibles:\n")
+    print("\t1. Télécharger un fichier en tuile")
+    print("\t2. Améliorer les images")
+    print("\t3. Océriser")
+    print("\t4. Nettoyer les données")
+    choice = input("\nQuel option souhaitez-vous choisir?  ")
+
+
+if __name__ == '__main__':
+    main()
