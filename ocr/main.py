66dd2bdeea4d95295d6752639b66067d5714a1f8
Joshua L.J. Vachon <joshua.vachon@cyberfjord.studio>
2023-11-18 12:41:30 -0500

    Ajout de la fonction de téléchargement

--- ocr/main.py
+++ ocr/main.py
@@ -10,8 +10,8 @@ def main():
     config = configparser.ConfigParser()
     config.sections()
     config.read('data/env.ini')
-    choice = 0
-    while choice != -1:
+    choice = "0"
+    while choice != "-1":
         print("Choix disponibles:\n")
         print("\t1. Télécharger un fichier en tuile")
         print("\t2. Améliorer les images")
@@ -19,20 +19,20 @@ def main():
         print("\t4. Nettoyer les données")
         choice = input("\nQuel option souhaitez-vous choisir?  ")
         match choice:
-            case 1:
+            case "1":
                 page_start = input("\n\tA partir de quel page?  ")
                 page_end = input("\n\tJusqu'a la page?  ")
                 FH.download(config["source"], page_start, page_end)
-            case 2:
+            case "2":
                 pass
-            case 3:
+            case "3":
                 pass
-            case 4:
+            case "4":
                 pass
-            case -1:
+            case "-1":
                 pass
             case _:
-                choice = 0
+                choice = "0"
 
 
 if __name__ == '__main__':
--- ocr/scripts/FilesHelper.py
+++ ocr/scripts/FilesHelper.py
@@ -1,5 +1,20 @@
-import configparser
+import subprocess
+import time
+import os
 
 
 def download(config, p1, p2):
-    pass
+    folder = str(int(time.time()))
+    os.mkdir(os.path.join(os.getcwd(), "input", folder))
+    print("⚪ Début des téléchargements")
+    for p in range(int(p1), int(p2) + 1):
+        dezoomify_path = os.path.join(os.getcwd(), "scripts", "dezoomify-rs.exe")
+        page = str(p).rjust(4, "0")
+
+        url = config["Template"].replace("||Page||", page)
+        output_path = os.path.join(os.getcwd(), "input", folder, page + ".jpg")
+        command = [dezoomify_path, "-l", url, output_path]
+
+        process = subprocess.run(command, capture_output=True, text=False)
+        print(f"▪️ ✅ {page}.jpg créé avec succès") if process.returncode == 0 else print(f"▪️ ❌ ERREUR: {process.stderr}")
+    print("⚫ Fin des téléchargements")
